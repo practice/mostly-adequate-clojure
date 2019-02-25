736433bc94ab8ac679cb23310d6ec75bdfa0c0a4
Shawn. Won <shawn.won@gmail.com>
2019-02-25 13:29:39 +0900

    Added some code. I don't remember.

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.9.1"]])
+                 [clj-http "3.9.1"]
+                 [org.clojure/algo.monads "0.1.6"]
+                 [funcool/cats "2.3.1"]
+                 ])
--- test/mostly_adequate_clojure/core_test.clj
+++ test/mostly_adequate_clojure/core_test.clj
@@ -1,7 +1,58 @@
 (ns mostly-adequate-clojure.core-test
   (:require [clojure.test :refer :all]
-            [mostly-adequate-clojure.core :refer :all]))
+            [mostly-adequate-clojure.core :refer :all]
+            [clojure.algo.monads :as am]
+            [cats.core :as m]
+            [cats.monad.either :as eit]
+            [cats.builtin]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn m-bind [value function]
+  (function value))
+
+(defn id-m [n]
+  (am/domonad am/identity-m
+    [a n
+     b (inc a)] (* a b)))
+
+(defn maybe-m [n]
+  (am/domonad am/maybe-m
+    [a n
+     b (inc a)] (* a b)))
+
+(deftest m-bind-test
+  (testing "m-bind test"
+    (is (= (m-bind 1 (fn [a]
+                     (m-bind (inc a) (fn [b]
+                                       (* a b))))) 2)))
+  (testing "identity-m"
+    (is (= 6 (id-m 2))))
+  (testing "identity-m fail"
+    (is (thrown? NullPointerException (id-m nil))))
+  (testing "maybe-m"
+    (is (= nil (maybe-m nil)))))
+
+(defn m-bind-first-try [sequence function]
+  (apply concat (map function sequence)))
+
+(m-bind-first-try (range 5)  (fn [a]
+                               (m-bind-first-try (range a)  (fn [b]
+                                                              (list (* a b))))))
+
+(map (fn [a]
+       (map (fn [b]
+              (list (* a b))) (range a))) (range 5))
+
+(am/with-monad am/sequence-m
+  (defn mystery
+    [f xs]
+    ( (am/m-lift 1 f) xs )))
+
+(mystery #(str "hello " %) (range 5))
+
+(am/with-monad am/sequence-m
+  (defn ntuples [n xs]
+    (am/m-seq (repeat n xs))))
+
+(ntuples 2 [1 2 3])
+
+(eit/right 200)
